ff6e9d25caecd6c8b9ca500540c71a35fb28cfb7
shujaatabdi <shoaib.hussain@mbeforyou.com>
2016-03-03 19:31:32 +0500

    Shoaib- Add EmpAdress BLL and DAL

---
+++ BAL/EmployeeAddressBLL.cs
@@ -0,0 +1,101 @@
+﻿using DAL;
+using Entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+  public  class EmpAddressBLL
+    {
+        public string LoadEmployeeAddress()
+        {
+            EmpAddressDAL ObjAll = new EmpAddressDAL();
+            return ObjAll.LoadEmployeeAddress();
+
+        }
+        public string EmployeeAddressAdd(EmpAddress Data)
+        {
+            string Status = "";
+            EmpAddressDAL Objall = new EmpAddressDAL();
+
+            SystemResponce ObjConst = new SystemResponce();
+            ValidationContext context = new ValidationContext(Data, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(Data, context, results, true);
+            if (!valid)
+            {
+                foreach (ValidationResult vr in results)
+                {
+                    //Response.Write(string.Format("Member Name :{0}", vr.MemberNames.First()));
+
+                    Status += string.Format("   ::  {0}{1}", vr.ErrorMessage, Environment.NewLine);
+                }
+                ObjConst.Msg = Status;
+            }
+            else
+            {
+                ObjConst = Objall.EmployeeAddressAdd(Data);
+            }
+            return JsonConvert.SerializeObject(ObjConst);
+        }
+
+        public string EditEmployeeAddress(EmpAddress Data)
+        {
+            //string Status = "";
+            EmpAddressDAL Objall = new EmpAddressDAL();
+
+            SystemResponce ObjConst = new SystemResponce();
+            ValidationContext context = new ValidationContext(Data, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(Data, context, results, true);
+            //if (!valid)
+            //{
+            //    foreach (ValidationResult vr in results)
+            //    {
+            //        //Response.Write(string.Format("Member Name :{0}", vr.MemberNames.First()));
+
+            //        Status += string.Format("   ::  {0}{1}", vr.ErrorMessage, Environment.NewLine);
+            //    }
+            //    ObjConst.Msg = Status;
+            //}
+            //else
+            //{
+            //Submit the form in database
+            ObjConst = Objall.EditEmployeeAddress(Data);
+
+            //}
+
+
+
+            return JsonConvert.SerializeObject(ObjConst);
+        }
+
+
+        public string GetEmployeeAddressForPaging(Paging Page)
+        {
+            EmpAddressDAL Objall = new EmpAddressDAL();
+            return Objall.GetEmployeeAddressForPaging(Page);
+        }
+
+        public string DeleteEmployeeAddress(EmpAddress Data)
+        {
+            EmpAddressDAL Objall = new EmpAddressDAL();
+
+            SystemResponce ObjConst = Objall.DeleteEmployeeAddress(Data);
+            return JsonConvert.SerializeObject(ObjConst);
+        }
+
+        public string GetEmployeeAddressByID(Paging page)
+        {
+            EmpAddressDAL Objall = new EmpAddressDAL();
+            return Objall.GetEmployeeAddressByID(page);
+        }
+
+
+    }
+}
---
+++ DAL/EmpAddressDAL.cs
@@ -0,0 +1,292 @@
+﻿using Entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class EmpAddressDAL
+    {
+
+        DBFactory dbFact = null;
+        DataProviderType dbType;
+        private static string connString = "";
+
+        public string LoadEmployeeAddress()
+        {
+            string result = "";
+            dbType = (DataProviderType)Enum.Parse(typeof(DataProviderType), ConfigurationManager.AppSettings["DatabaseType"].ToString());
+            connString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            dbFact = new DBFactory(dbType, connString);
+
+            DBConnection dbCon = dbFact.GetDataAccessLayer();
+            if (dbCon.OpenConnection(dbFact.ConnectionString))
+            {
+                DataSet ds = dbCon.ExecuteFetchCommand("SpEmployeeAddressGetAll", CommandType.StoredProcedure);
+                //if (ds.Tables[0].Rows.Count > 0)
+                //{
+                //    // success
+                //    result = "<xml>" + String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>{2}", Constants.SUCCESS, "", ds.GetXml()) + "</xml>";
+                //}
+                //else
+                //{
+                //    if (String.IsNullOrEmpty(dbCon.GetLastError))
+                //        result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>{2}", Constants.EMPTY, Constants.MessageEmpty, ds.GetXml());
+                //    else
+                //        result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>", Constants.FAILURE, dbCon.GetLastError);
+
+                //}
+                result = Converter(ds.Tables[0]);
+            }
+            else
+            {
+                result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>", Constants.FAILURE, Constants.MessageDBConnectivityFailure);
+                //throw new ArgumentException(dbCon.GetLastError);
+            }
+
+            return result;
+        }
+        public SystemResponce EmployeeAddressAdd(EmpAddress ObjEmpAdd)
+        {
+            SystemResponce result = new SystemResponce();
+            Hashtable ht = new Hashtable();
+
+            ht.Add("@EmpID", ObjEmpAdd.EmployeeID);
+            ht.Add("@Address", ObjEmpAdd.Address);
+            ht.Add("@Ph1", ObjEmpAdd.Phone1);
+            ht.Add("@Ph2", ObjEmpAdd.Phone2);
+            ht.Add("@Status", ObjEmpAdd.Status);
+            dbType = (DataProviderType)Enum.Parse(typeof(DataProviderType), ConfigurationManager.AppSettings["DatabaseType"].ToString());
+            connString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            dbFact = new DBFactory(dbType, connString);
+
+            DBConnection dbCon = dbFact.GetDataAccessLayer();
+            if (dbCon.OpenConnection(dbFact.ConnectionString))
+            {
+                int res = Convert.ToInt32(dbCon.ExecuteScalarParameterized("[dbo].[SpEmployeeAddressAddNew]", CommandType.StoredProcedure, ht));
+                if (res == -1)
+                {
+                    result.ErrorCode = Constants.AlreadExists;
+                    result.Msg = Constants.MessageAlreadExists;
+                }
+                else
+                {
+                    result.ErrorCode = Constants.SUCCESS;
+                    result.Msg = Constants.MessageSuccessSave;
+                }
+            }
+            else
+            {
+                result.ErrorCode = Constants.FAILURE;
+                result.Msg = Constants.MessageDBConnectivityFailure;
+            }
+
+            return result;
+        }
+
+        public string GetEmployeeAddressForPaging(Paging Page)
+        {
+            string result = "";
+            Hashtable ht = new Hashtable();
+            ht.Add("@PageNumber", Page.PageNo);
+            ht.Add("@RecordPP", Page.RecordPerPage);
+            ht.Add("@SortBy", Page.SortBy);
+            ht.Add("@SortAs", Page.SortAs);
+            ht.Add("@SearchBy", Page.SearchBy);
+            ht.Add("@SearchVal", Page.SearchVal);
+            if (Page.Extra != null)
+            {
+                //foreach (var item in Page.Extra.Keys)
+                //{
+                //    ht.Add(item.ToString(), Page.Extra[item].ToString());
+                //}
+                for (int i = 0; i < Page.Extra.Length; i++)
+                {
+                    ht.Add(Page.Extra[i].Pname, Page.Extra[i].Pval);
+                }
+            }
+
+            dbType = (DataProviderType)Enum.Parse(typeof(DataProviderType), ConfigurationManager.AppSettings["DatabaseType"].ToString());
+            connString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            dbFact = new DBFactory(dbType, connString);
+
+            DBConnection dbCon = dbFact.GetDataAccessLayer();
+            if (dbCon.OpenConnection(dbFact.ConnectionString))
+            {
+                DataSet ds = dbCon.ExecuteFetchCommandParameterized("SpEmployeeAddressGetAllPaging", CommandType.StoredProcedure, ht);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    // success
+                    result = "<xml>" + String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>{2}", Constants.SUCCESS, "", ds.GetXml()) + "</xml>";
+                }
+                else
+                {
+                    if (String.IsNullOrEmpty(dbCon.GetLastError))
+                        result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>{2}", Constants.EMPTY, Constants.MessageEmpty, ds.GetXml());
+                    else
+                        result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>", Constants.FAILURE, dbCon.GetLastError);
+
+                }
+                result = Converter(ds.Tables[0]) + "@@" + Converter(ds.Tables[1]);
+
+
+            }
+            else
+            {
+                result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>", Constants.FAILURE, Constants.MessageDBConnectivityFailure);
+                throw new ArgumentException(dbCon.GetLastError);
+            }
+
+            return result;
+        }
+
+        private string Converter(DataTable table)
+        {
+            string JSONString = string.Empty;
+            JSONString = JsonConvert.SerializeObject(table);
+            return JSONString;
+        }
+
+        public SystemResponce DeleteEmployeeAddress(EmpAddress ObjEmpAdd)
+        {
+            SystemResponce result = new SystemResponce();
+            Hashtable ht = new Hashtable();
+
+            ht.Add("@EmployeeAddressID", ObjEmpAdd.ID);
+
+
+            dbType = (DataProviderType)Enum.Parse(typeof(DataProviderType), ConfigurationManager.AppSettings["DatabaseType"].ToString());
+            connString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            dbFact = new DBFactory(dbType, connString);
+
+            DBConnection dbCon = dbFact.GetDataAccessLayer();
+            if (dbCon.OpenConnection(dbFact.ConnectionString))
+            {
+                int res = (int)dbCon.ExecuteScalarParameterized("SpEmployeeAddressDelete", CommandType.StoredProcedure, ht);
+                if (res == -1)
+                {
+                    result.ErrorCode = Constants.AlreadExists;
+                    result.Msg = Constants.MessageAlreadExists;
+                }
+                else
+                {
+                    result.ErrorCode = Constants.SUCCESS;
+                    result.Msg = Constants.MessageSuccessDelete;
+                }
+            }
+            else
+            {
+                result.ErrorCode = Constants.FAILURE;
+                result.Msg = Constants.MessageDBConnectivityFailure;
+            }
+
+            return result;
+        }
+
+
+        public string GetEmployeeAddressByID(Paging Page)
+        {
+            string result = "";
+            Hashtable ht = new Hashtable();
+            //ht.Add("@PageNumber", Page.PageNo);
+            //ht.Add("@RecordPP", Page.RecordPerPage);
+            //ht.Add("@SortBy", Page.SortBy);
+            //ht.Add("@SortAs", Page.SortAs);
+            //ht.Add("@SearchBy", Page.SearchBy);
+            //ht.Add("@SearchVal", Page.SearchVal);
+            if (Page.Extra != null)
+            {
+                //foreach (var item in Page.Extra.Keys)
+                //{
+                //    ht.Add(item.ToString(), Page.Extra[item].ToString());
+                //}
+                for (int i = 0; i < Page.Extra.Length; i++)
+                {
+                    ht.Add(Page.Extra[i].Pname, Page.Extra[i].Pval);
+                }
+            }
+
+            dbType = (DataProviderType)Enum.Parse(typeof(DataProviderType), ConfigurationManager.AppSettings["DatabaseType"].ToString());
+            connString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            dbFact = new DBFactory(dbType, connString);
+
+            DBConnection dbCon = dbFact.GetDataAccessLayer();
+            if (dbCon.OpenConnection(dbFact.ConnectionString))
+            {
+                DataSet ds = dbCon.ExecuteFetchCommandParameterized("SpEmployeeAddressByID", CommandType.StoredProcedure, ht);
+                //if (ds.Tables[0].Rows.Count > 0)
+                //{
+                //    // success
+                //    result = "<xml>" + String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>{2}", Constants.SUCCESS, "", ds.GetXml()) + "</xml>";
+                //}
+                //else
+                //{
+                //    if (String.IsNullOrEmpty(dbCon.GetLastError))
+                //        result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>{2}", Constants.EMPTY, Constants.MessageEmpty, ds.GetXml());
+                //    else
+                //        result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>", Constants.FAILURE, dbCon.GetLastError);
+
+                //}
+                result = Converter(ds.Tables[0]);
+
+
+            }
+            else
+            {
+                result = String.Format("<Response><Code>{0}</Code><Message>{1}</Message></Response>", Constants.FAILURE, Constants.MessageDBConnectivityFailure);
+                //throw new ArgumentException(dbCon.GetLastError);
+            }
+
+            return result;
+        }
+
+
+
+
+        public SystemResponce EditEmployeeAddress(EmpAddress ObjEmpAdd)
+        {
+            SystemResponce result = new SystemResponce();
+            Hashtable ht = new Hashtable();
+            ht.Add("@ID", ObjEmpAdd.ID);
+            ht.Add("@EmpID", ObjEmpAdd.EmployeeID);
+            ht.Add("@Address", ObjEmpAdd.Address);
+            ht.Add("@Ph1", ObjEmpAdd.Phone1);
+            ht.Add("@Ph2", ObjEmpAdd.Phone2);
+           
+            dbType = (DataProviderType)Enum.Parse(typeof(DataProviderType), ConfigurationManager.AppSettings["DatabaseType"].ToString());
+            connString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            dbFact = new DBFactory(dbType, connString);
+
+            DBConnection dbCon = dbFact.GetDataAccessLayer();
+            if (dbCon.OpenConnection(dbFact.ConnectionString))
+            {
+                //(int)dbCon.ExecuteScalarParameterized("SpShiftEdit", CommandType.StoredProcedure, ht);
+                int res = (int)dbCon.ExecuteScalarParameterized("SpEmployeeAddressEdit", CommandType.StoredProcedure, ht);
+                if (res == -1)
+                {
+                    result.ErrorCode = Constants.AlreadExists;
+                    result.Msg = Constants.MessageAlreadExists;
+                }
+                else
+                {
+                    result.ErrorCode = Constants.SUCCESS;
+                    result.Msg = Constants.MessageSuccessUpate;
+                }
+            }
+            else
+            {
+                result.ErrorCode = Constants.FAILURE;
+                result.Msg = Constants.MessageDBConnectivityFailure;
+            }
+
+            return result;
+        }
+
+    }
+}
