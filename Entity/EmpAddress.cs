2c6f2f54fd564e225f3232e0bf5311467fd038e6
Shujaatabdi <dotssabdi@gmail.com>
2016-03-03 20:07:46 +0500

    Shujaat- update EmpAddress Entity validation msg

--- Entity/EmpAddress.cs
+++ Entity/EmpAddress.cs
@@ -14,13 +14,13 @@ namespace Entity
         
         public string EmployeeID { get; set; }
         [Required(ErrorMessage = "Address is required")]
-        [RegularExpression(@"^[a-zA-Z\s][a-zA-Z0-9 .-/]*$", ErrorMessage = "insert Valid BranchID I.e no Special Charactors")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minimum 3 and maximum 99 characters")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Address should be minimum 3 and maximum 99 characters")]
         [DataType(DataType.Text)]
         public string Address { get; set; }
+       
         [Required(ErrorMessage = "Phone Number 1 is required")]
         [RegularExpression(@"^[0-9]*$", ErrorMessage = "insert Valid Phone 1 I.e no Special Charactors")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minimum 3 and maximum 99 characters")]
+
         [DataType(DataType.Text)]
         public string Phone1 { get; set; }
         [Required(ErrorMessage = "Phone Number 2 is required")]
